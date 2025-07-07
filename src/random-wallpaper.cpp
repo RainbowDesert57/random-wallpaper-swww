48432400a4a707ef112cb7585b32ef9b65960f45
Tanish <tanishj1711@gmail.com>
2025-07-07 18:34:43 +0530

    Added src with random wallpaper

---
+++ src/random-wallpaper.cpp
@@ -0,0 +1,65 @@
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+int main() {
+	//random number each time the program runs
+//intro
+cout<<"Default wallpaper directory: ~/wallpapers"<<endl<<"Change directory if something else."<<endl<<"list the wallpaper names for random wallpapers. NOTE: will be automatic in the future"
+cout<<"██╗    ██╗ █████╗ ██╗     ██╗     ██████╗  █████╗ ██████╗ ███████╗██████╗";
+cout<<"██║    ██║██╔══██╗██║     ██║     ██╔══██╗██╔══██╗██╔══██╗██╔════╝██╔══██╗";
+cout<<"██║ █╗ ██║███████║██║     ██║     ██████╔╝███████║██████╔╝█████╗  ██████╔╝";
+cout<<"██║███╗██║██╔══██║██║     ██║     ██╔═══╝ ██╔══██║██╔═══╝ ██╔══╝  ██╔══██╗";
+cout<<"╚███╔███╔╝██║  ██║███████╗███████╗██║     ██║  ██║██║     ███████╗██║  ██║";
+cout<<" ╚══╝╚══╝ ╚═╝  ╚═╝╚══════╝╚══════╝╚═╝     ╚═╝  ╚═╝╚═╝     ╚══════╝╚═╝  ╚═╝";
+cout<<"1. Choose a wallpaper";
+cout<<"2. Random wallpaper"
+
+
+	srand(time(0));
+	int randRange = 14; // 0 to randRange
+	int randNum = (rand() % randRange) + 1;
+	if (randNum == 1) {
+		system("swww img ~/wallpapers/astronaut.jpg"); //wallpaper output
+	}
+	else if (randNum == 2) {
+		system("swww img ~/wallpapers/sky.jpg"); //wallpaper output
+	}
+	else if (randNum == 3) {
+		system("swww img ~/wallpapers/cat.jpg"); //wallpaper output
+	}
+	else if (randNum == 4) {
+		system("swww img ~/wallpapers/city.jpg"); //wallpaper output
+	}
+	else if (randNum == 5) {
+		system("swww img ~/wallpapers/forestBlueWallpaper.jpg"); //wallpaper output
+	}
+	else if (randNum == 6) {
+		system("swww img ~/wallpapers/BSD1"); //wallpaper output
+	}
+	else if (randNum == 7) {
+		system("swww img ~/wallpapers/BSD2"); //wallpaper output
+	}
+	else if (randNum == 8) {
+		system("swww img ~/wallpapers/BSD3"); //wallpaper output
+	}
+	else if (randNum == 9) {
+		system("swww img ~/wallpapers/hong-kong-night.jpg"); //wallpaper output
+	}
+	else if (randNum == 10) {
+		system("swww img ~/wallpapers/japan.jpg"); //wallpaper output
+	}
+	else if (randNum == 11) {
+		system("swww img ~/wallpapers/nightSky.jpg"); //wallpaper output
+	}
+	else if (randNum == 12) {
+		system("swww img ~/wallpapers/purpleCity.jpg"); //wallpaper output
+	}
+	else if (randNum == 13) {
+		system("swww img ~/wallpapers/purpleSky.jpg"); //wallpaper output
+	}
+	else if (randNum == 14) {
+		system("swww img ~/wallpapers/samuraiDragon.jpg"); //wallpaper output
+	}
+	cout<<"Changing To A Random Wallpaper..."<<endl;
+	cout<<"Exitting..."<<endl<<endl;
+}
